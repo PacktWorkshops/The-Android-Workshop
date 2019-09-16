b5225aea4aa81cf966d5b155bffa589e9d0fbd8d
alexforrester <aforresterwork@gmail.com>
2019-09-16 21:43:16 +0100

    Adds MyApplication example app and minor changes to colors app

---
+++ Chapter1/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        Exercise 3 Loading Webview
+//        val webView = WebView(this)
+//        setContentView(webView)
+//        webView.loadUrl("https://www.google.com")
+    }
+
+
+}
+
+
+
